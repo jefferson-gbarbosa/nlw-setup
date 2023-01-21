d51a7a7d9ff0ade41da60cf3c3f8e28cd5f7ce6b
jefferson-gbarbosa <jeffersonqx@gmail.com>
2023-01-21 15:37:07 -0300

    addition of the alert modal in the project

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
+const modalContainer = document.querySelector('.modal-container');
+const closeBtn = document.querySelector('#close-btn');
 
 button.addEventListener('click',add);
 form.addEventListener('change',save);
+closeBtn.addEventListener('click',removeModal);
 
-
+// add habits
 function add(){
     const today = new Date().toLocaleDateString('pt-br').slice(0,-5);
 
     const dayExists = nlwSetup.dayExists(today);
 
     if(dayExists){
-        alert('Dia já incluso');
+        // alert('Dia já incluso');
+        error();
         return;
     }
     nlwSetup.addDay(today);
 }
-
 function save(){
     localStorage.setItem('NLWSetup@habits',JSON.stringify(nlwSetup.data))
 }
@@ -26,3 +29,21 @@ const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 
 nlwSetup.setData(data);
 nlwSetup.load();
+
+
+// function alert
+function error(){
+    modalContainer.classList.add('show');
+}
+// remove alert
+function removeModal(){
+    modalContainer.classList.remove('show');
+}
+// remove touch alert anywhere on screen
+window.addEventListener('click', function(e) {
+
+    if (e.target === modalContainer) {
+        modalContainer.classList.remove('show');
+    }
+});
+
